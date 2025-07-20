bd6406724438231358d26a19e08a80303b8e9b61
Ahmed Maged <mageda9000@gmail.com>
2025-07-20 23:44:56 +0300

    add gather and scatter operations

--- include/load_store.hpp
+++ include/load_store.hpp
@@ -9,7 +9,7 @@ namespace qlm
     namespace vec
     {
         /*
-            linear load function that loads data from a pointer into a VecRegister
+            linear load/store function that loads data from a pointer into a VecRegister
         */
         template< int simd_size, Primitive T, int mask_size>
         requires ValidSIMDFunction<T, simd_size, mask_size>
@@ -33,9 +33,6 @@ namespace qlm
             return Load<simd_size>(ptr, mask);
         }
 
-        /*
-            linear store function that stores data from a VecRegister to a pointer
-        */
         template< int simd_size, Primitive T, int mask_size>
         requires ValidSIMDFunction<T, simd_size, mask_size>
         void Store(T*const ptr, const VecRegister<T, simd_size>& vec, const MaskRegister<mask_size>& mask)
@@ -54,6 +51,51 @@ namespace qlm
             constexpr int num_bits = simd_size / (sizeof(T) * 8);
             const MaskRegister<num_bits> mask(true);
             Store(ptr, vec, mask);
+        }
+
+        /*
+            gather load/store function that loads data from a pointer into a VecRegister
+        */
+        template< int simd_size, Primitive T, int mask_size>
+        requires ValidSIMDFunction<T, simd_size, mask_size>
+        VecRegister<T, simd_size> Gather(const T*const ptr, const VecRegister<int, simd_size>& add, const MaskRegister<mask_size>& mask)
+        {
+            qlm::VecRegister<T, simd_size> vec;
+            for (int i = 0; i < vec.num_elements; i++)
+            {
+                if (mask[i])
+                    vec[i] = ptr[add[i]];
+            }
+            return vec;
+        }
+
+        template< int simd_size, Primitive T>
+        requires ValidSIMDWidth<simd_size>
+        VecRegister<T, simd_size> Gather(const T*const ptr, const VecRegister<int, simd_size>& add)
+        {
+            constexpr int num_bits = simd_size / (sizeof(T) * 8);
+            const MaskRegister<num_bits> mask(true);
+            return Gather<simd_size>(ptr, add, mask);
+        }
+
+        template< int simd_size, Primitive T, int mask_size>
+        requires ValidSIMDFunction<T, simd_size, mask_size>
+        void Scatter(T*const ptr, const VecRegister<T, simd_size>& vec, const VecRegister<int, simd_size>& add, const MaskRegister<mask_size>& mask)
+        {
+            for (int i = 0; i < vec.num_elements; i++)
+            {
+                if (mask[i])
+                    ptr[add[i]] = vec[i];
+            }
+        }
+
+        template< int simd_size, Primitive T>
+        requires ValidSIMDWidth<simd_size>
+        void Scatter(T*const ptr, const VecRegister<T, simd_size>& vec, const VecRegister<int, simd_size>& add)
+        {
+            constexpr int num_bits = simd_size / (sizeof(T) * 8);
+            const MaskRegister<num_bits> mask(true);
+            Scatter(ptr, vec, add, mask);
         }
     }
 }
