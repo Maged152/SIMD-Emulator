f245dbe625deb7210d807fce57f91ffa3555eede
Ahmed Maged <mageda9000@gmail.com>
2025-07-22 23:33:07 +0300

    refactor vector operations to use a generic Apply function; add Print function for VecRegister

--- include/arithmetic.hpp
+++ include/arithmetic.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "load_store.hpp"
+#include <functional>
 
 
 namespace qlm
@@ -8,28 +9,63 @@ namespace qlm
     namespace vec
     {
         /*
-            vector addition function that adds two VecRegisters element-wise
+            Common element-wise vector operation with mask
+            op: a callable with signature T(T, T)
         */
-        template<int simd_size, Primitive T, int mask_size>
+        template<int simd_size, Primitive T, int mask_size, typename Op>
         requires ValidSIMDFunction<T, simd_size, mask_size>
-        VecRegister<T, simd_size> Add(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1, const MaskRegister<mask_size>& mask)
+        VecRegister<T, simd_size> Apply(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1, const MaskRegister<mask_size>& mask, Op op)
         {
             qlm::VecRegister<T, simd_size> res;
             for (int i = 0; i < vec0.num_elements; i++)
             {
                 if (mask[i])
-                    res[i] = vec0[i] + vec1[i];
+                    res[i] = op(vec0[i], vec1[i]);
             }
             return res;
+        }
+
+        template<int simd_size, Primitive T, typename Op>
+        requires ValidSIMDWidth<simd_size>
+        VecRegister<T, simd_size> Apply(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1, Op op)
+        {
+            constexpr int num_bits = simd_size / (sizeof(T) * 8);
+            const MaskRegister<num_bits> mask(true);
+            return Apply(vec0, vec1, mask, op);
+        }
+
+        /*
+            vector addition
+        */
+        template<int simd_size, Primitive T, int mask_size>
+        requires ValidSIMDFunction<T, simd_size, mask_size>
+        VecRegister<T, simd_size> Add(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1, const MaskRegister<mask_size>& mask)
+        {
+            return Apply(vec0, vec1, mask, std::plus<T>());
         }
 
         template<int simd_size, Primitive T>
         requires ValidSIMDWidth<simd_size>
         VecRegister<T, simd_size> Add(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1)
         {
-            constexpr int num_bits = simd_size / (sizeof(T) * 8);
-            const MaskRegister<num_bits> mask(true);
-            return Add(vec0, vec1, mask);
+            return Apply(vec0, vec1, std::plus<T>());
+        }
+
+        /*
+            vector multiplication
+        */
+       template<int simd_size, Primitive T, int mask_size>
+        requires ValidSIMDFunction<T, simd_size, mask_size>
+        VecRegister<T, simd_size> Mul(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1, const MaskRegister<mask_size>& mask)
+        {
+            return Apply(vec0, vec1, mask, std::multiplies<T>());
+        }
+
+        template<int simd_size, Primitive T>
+        requires ValidSIMDWidth<simd_size>
+        VecRegister<T, simd_size> Mul(const VecRegister<T, simd_size>& vec0, const VecRegister<T, simd_size>& vec1)
+        {
+            return Apply(vec0, vec1, std::multiplies<T>());
         }
     }
 }
---
+++ include/simd_debug.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "load_store.hpp"
+#include <iostream>
+
+
+namespace qlm
+{
+    namespace vec
+    {
+        template<typename T, int simd_size>
+        void Print(const char* name, const qlm::VecRegister<T, simd_size>& vec)
+        {
+            std::cout << name << ": [";
+            for (int i = 0; i < vec.num_elements - 1; ++i) 
+                std::cout << vec[i] << ", ";
+            
+            std::cout << vec[vec.num_elements - 1] << "]\n";
+        } 
+    }
+}
