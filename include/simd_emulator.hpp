f14da9436b6f34426f97711cf71c866a386d5610
Ahmed Maged <mageda9000@gmail.com>
2025-08-04 20:40:34 +0300

    add Shuffle

--- include/simd_emulator.hpp
+++ include/simd_emulator.hpp
@@ -3,3 +3,4 @@
 #include "simd_arithmetic.hpp"
 #include "simd_operators.hpp"
 #include "simd_reduction.hpp"
+#include "simd_miscellaneous.hpp"
---
+++ include/simd_miscellaneous.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "vector_register.hpp"
+#include "mask_register.hpp"
+
+
+namespace qlm
+{
+    namespace vec
+    {
+        template<int simd_size, Primitive T>
+        requires ValidSIMDWidth<simd_size>
+        VecRegister<T, simd_size> Shuffle(const VecRegister<T, simd_size>& vec, const VecRegister<int, simd_size>& indices)
+        {
+            VecRegister<T, simd_size> res;
+            for (int i = 0; i < vec.num_elements; ++i)
+            {
+                const int idx = indices[i];
+                res[i] = (idx >= 0 && idx < vec.num_elements) ? vec[idx] : vec[i];
+            }
+            
+            return res;
+        }
+
+    }
+}
