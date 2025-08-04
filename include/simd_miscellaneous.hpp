cb69187474f6bb3b7d4ccef0b79001274ac5d3bb
Ahmed Maged <mageda9000@gmail.com>
2025-08-04 20:42:18 +0300

    rename

--- include/simd_miscellaneous.hpp
+++ include/simd_miscellaneous.hpp
@@ -10,7 +10,7 @@ namespace qlm
     {
         template<int simd_size, Primitive T>
         requires ValidSIMDWidth<simd_size>
-        VecRegister<T, simd_size> Shuffle(const VecRegister<T, simd_size>& vec, const VecRegister<int, simd_size>& indices)
+        VecRegister<T, simd_size> Permute(const VecRegister<T, simd_size>& vec, const VecRegister<int, simd_size>& indices)
         {
             VecRegister<T, simd_size> res;
             for (int i = 0; i < vec.num_elements; ++i)
@@ -18,7 +18,7 @@ namespace qlm
                 const int idx = indices[i];
                 res[i] = (idx >= 0 && idx < vec.num_elements) ? vec[idx] : vec[i];
             }
-            
+
             return res;
         }
 
