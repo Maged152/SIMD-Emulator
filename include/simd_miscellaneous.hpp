771370c9889c90e3f905cdb3f7508c34b819c32e
Ahmed Maged <mageda9000@gmail.com>
2025-08-04 20:46:48 +0300

    add select instruction

--- include/simd_miscellaneous.hpp
+++ include/simd_miscellaneous.hpp
@@ -22,5 +22,18 @@ namespace qlm
             return res;
         }
 
+        template<int simd_size, Primitive T>
+        requires ValidSIMDWidth<simd_size>
+        VecRegister<T, simd_size> Select(const VecRegister<T, simd_size>& vec_true,  const VecRegister<T, simd_size>& vec_false,  const MaskRegister<MaskSize<T, simd_size>>& mask)
+        {
+            VecRegister<T, simd_size> res;
+            for (int i = 0; i < vec_true.num_elements; ++i)
+            {
+                res[i] = mask[i] ? vec_true[i] : vec_false[i];
+            }
+            
+            return res;
+        }
+
     }
 }
